353505972f97082879b1f8a0bfb6813290a42a31
Tiago Rodrigues <isatiago098@gmail.com>
2021-09-13 16:55:19 -0300

    resoluçao exercicio soma2numeros

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace soma2numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("soma 2 numeros");
+int x = 30;
+int y = 40;
+int soma = x + y;
+Console.WriteLine($"{x} + {y} = {soma}");
+        }
+    }
+}
